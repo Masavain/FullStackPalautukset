12cefc9b75265483be18bb41a730ade6b7f72e51
Masavain <matti.vainionpaa@helsinki.fi>
2018-01-19 21:51:12 +0200

    1.7 final

--- 1.6-1.11/src/index.js
+++ 1.6-1.11/src/index.js
@@ -8,29 +8,58 @@ class App extends React.Component {
             hyva: 0,
             neutraali: 0,
             huono: 0,
-            kaikki: []
+            kaikkia: 0
         }
     }
 
     klikHyva = () => {
         this.setState({
             hyva: this.state.hyva + 1,
+            kaikkia: this.state.kaikkia + 1
         })
     }
 
     klikNeut = () => {
         this.setState({
             neutraali: this.state.neutraali + 1,
+            kaikkia: this.state.kaikkia + 1
+
         })
     }
 
     klikHuono = () => {
         this.setState({
             huono: this.state.huono + 1,
+            kaikkia: this.state.kaikkia + 1
+
         })
     }
 
+
+
     render() {
+        const keskiarvo = () => {
+            if (this.state.kaikkia === 0) {
+                return (
+                    <div>
+                        keskiarvo 0
+                    </div>
+                )
+            }
+            return (
+                <div>
+                    keskiarvo {Math.round(((this.state.hyva - this.state.huono) / this.state.kaikkia) * 100) / 100}
+                </div>
+            )
+        }
+
+        const positiivisia = () => {
+            return (
+                <div>
+                    positiivisia {Math.round(((this.state.hyva) / this.state.kaikkia) * 100) * 100 / 100} %
+            </div>
+            )
+        }
 
         return (
             <div>
@@ -46,14 +75,19 @@ class App extends React.Component {
                     <h2>statistiikka</h2>
                 </div>
                 <div>
-                   <p>hyvä {this.state.hyva}</p>
-                   <p>neutraali {this.state.neutraali}</p>
-                   <p>huono {this.state.huono}</p>
+                    <div>hyvä {this.state.hyva}</div>
+                    <div>neutraali {this.state.neutraali}</div>
+                    <div>huono {this.state.huono}</div>
+                    {keskiarvo()}
+                    {positiivisia()}
                 </div>
             </div>
+
         )
     }
 }
+
+
 
 
 ReactDOM.render(
